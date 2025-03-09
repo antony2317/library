56a63dd48a666934863f2204c159aa031e72b501
antony2317 <anton43234@gmail.com>
2025-03-09 23:30:32 +0300

    библиотека

--- mylib/mylib/settings.py
+++ mylib/mylib/settings.py
@@ -117,14 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'user', 'static'),
-]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
--- mylib/mylib/urls.py
+++ mylib/mylib/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('users/', include('user.urls', namespace='users')),
-]  + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
